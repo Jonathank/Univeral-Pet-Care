5cd7f49805ba5d6fed5439f1ffbd1b984b7f76ea
unknown <kyeyunejonathan001@gmail.com>
2025-05-06 11:43:26 +0300

    feat: added User superclass Entity with the inheritance strategy of joined to manage all the subclasses commion properties.  -modified the subclasses extending the superclass User to inherit the properties of Superclass

---
+++ src/main/java/com/jonathanK/universalpetcare/model/User.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.jonathanK.universalpetcare.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author JONATHAN
+ */
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    @Column(name = "mobile")
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String userTpe;
+    private boolean isEnabled;
+}
--- src/main/java/com/jonathanK/universalpetcare/model/Veterinarian.java
+++ src/main/java/com/jonathanK/universalpetcare/model/Veterinarian.java
@@ -3,9 +3,27 @@
  */
 package com.jonathanK.universalpetcare.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author JONATHAN
  */
-public class Veterinarian {
-
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "vet_Id")
+public class Veterinarian extends User{
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String specialization;
+    
 }
